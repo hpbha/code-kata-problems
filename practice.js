7a274b20521d45def74d03312dad56fe144f7b78
hpbha <50054755+hpbha@users.noreply.github.com>
2021-03-18 16:12:54 +0530

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,1 +1,27 @@
-console.log("absolute beginer")
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+  let num = userInput[0].split("").map((ele)=>{
+      return parseInt(ele);
+        }
+      )
+ let sum =0;
+ num.forEach((number)=>{
+     sum = sum + number;
+ })
+ console.log(sum)
+  //end-here
+});
